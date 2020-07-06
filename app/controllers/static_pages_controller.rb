9835731d07f374e5b43586d2545d8c8d33016ede
miki <pikkoro-pacemaker@hotmail.co.jp>
2020-07-06 15:13:37 +0900

    header修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @recipes = current_user.recipes.all
+      render "recipes/index", recipes: @recipes
+    end
   end
 end
